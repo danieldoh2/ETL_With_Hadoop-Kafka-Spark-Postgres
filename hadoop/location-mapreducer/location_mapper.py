0ab45aac9fd92bf2ba7126cafd7d79b0980b5486
danieldoh2 <danieldoh.inbox@gmail.com>
2024-04-03 20:57:41 -0400

    mapping function changes

--- hadoop/location-mapreducer/location_mapper.py
+++ hadoop/location-mapreducer/location_mapper.py
@@ -5,15 +5,13 @@
 # write some useful code here and print to STDOUT
 import sys
 
+
 def map_reduce_with_stdin():
 
     for line in sys.stdin:
         words = line.strip().split(',')
-        print(words[1], '\t', 1)
         print(words[3], '\t', 1)
 
 
 if __name__ == '__main__':
     map_reduce_with_stdin()
-
-    
